2399e3a148d3d027ecac48551f7d4468e6f117aa
mmoerz <michael@moerz.co.at>
2020-08-06 17:47:20 +0000

    working version with macvlan

--- smb-ad-dc/tools/smb_test_dns.sh
+++ smb-ad-dc/tools/smb_test_dns.sh
@@ -2,6 +2,6 @@
 
 host -t SRV _ldap._tcp.$SAMBA_DC_REALM
 host -t SRV _kerberos._udp.$SAMBA_DC_REALM
-host $SAMBA_DC_HOSTNAME$SAMBA_DC_REALM
+host $SAMBA_DC_HOSTNAME.$SAMBA_DC_REALM
 
-sudo samba_dnsupdate --verbose --all-names
+#samba_dnsupdate --use-samba-tool --verbose --all-names
