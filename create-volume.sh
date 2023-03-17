55462a779996daed9b42f76bf13198038badc404
mmoerz <michael@moerz.co.at>
2023-03-17 16:56:45 +0100

    * making docker-compose file more maintainable

---
+++ create-volume.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+cat <<EOF
+USAGE: $0 make
+	creates directories and registers them for docker use
+EOF
+exit 1
+fi 
+
+createDirsAndRegisterWithDocker() {
+	DIRBASE=$1
+	DIRS=$2
+	for dcd in $DIRS; do
+		#mkdir -p $dcd
+		echo docker volume create --driver local \
+		--opt type=none \
+		--opt device=`pwd`/$DIRBASE/$dcd \
+		--opt o=bind samba_${DIRBASE}_${dcd/\//}
+	done
+}
+
+# dc1 directories
+createDirsAndRegisterWithDocker "dc1" "etc var/lib var/log"
+# fileserver directories
+createDirsAndRegisterWithDocker "fs1" "etc var/lib var/log supervisord shares"
+
+exit 0
+
