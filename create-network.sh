acff9e2e2dd6e4371d6f6eb391cdf868578b2595
Michael Moerz <michael@moerz.co.at>
2023-04-29 12:02:05 +0200

    + adding network creation to make it independent from docker-compose.yml

---
+++ create-network.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This is a pre-flight scritp to setup the network 
+#
+
+docker network create -d macvlan \
+    --subnet=192.168.1.0/24 \
+    --gateway=192.168.1.1  \
+    -o parent=br0 samba-network
+
