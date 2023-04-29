ae8e56155cd07b9f8a5688ba108dafcfd5d13745
Michael Moerz <michael@moerz.co.at>
2023-04-29 13:42:54 +0200

    + root access scripts and preflight lan config

--- create-network.sh
+++ create-network.sh
@@ -5,5 +5,6 @@
 docker network create -d macvlan \
     --subnet=192.168.1.0/24 \
     --gateway=192.168.1.1  \
+    --ip-range=192.168.1.0/24 \
     -o parent=br0 samba-network
 
--- root_samba.sh
+++ root_samba.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DOCKER_ID=$(docker container list | grep "mmoerz/docker-samba-ad-dc" | cut -d' ' -f1)
+DOCKER_ID=$(docker container list | grep "mmoerz/docker-samba-ad-dc" | grep -v "member" | cut -d' ' -f1)
 
 docker exec -it $DOCKER_ID /bin/bash
