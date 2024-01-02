9569125276a8a01a8902e7517a468ca2b1be9487
Michael Moerz <michael@moerz.co.at>
2024-01-03 00:00:23 +0100

    * enhanced script for multi host usage

--- create-volume.sh
+++ create-volume.sh
@@ -1,29 +1,47 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
+usage() {
 cat <<EOF
-USAGE: $0 make
+USAGE: $0 [addc|member] name 
 	creates directories and registers them for docker use
 EOF
-exit 1
+}
+
+if [ $# -ne 2 ]; then
+  usage
+  exit 1
 fi 
 
 createDirsAndRegisterWithDocker() {
 	DIRBASE=$1
 	DIRS=$2
 	for dcd in $DIRS; do
+    echo "creating dir $DIRBASE/$dcd"
 		mkdir -p $DIRBASE/$dcd
+    #echo "binding $DIRBASE/$dcd to samba_${DIRBASE}_${dcd/\///}"
+    echo -n "binding $DIRBASE/$dcd to:"
 		docker volume create --driver local \
 		--opt type=none \
 		--opt device=`pwd`/$DIRBASE/$dcd \
 		--opt o=bind samba_${DIRBASE}_${dcd/\//}
+    echo .
 	done
 }
 
-# dc1 directories
-createDirsAndRegisterWithDocker "dc1" "etc var/lib var/log"
-# fileserver directories
-createDirsAndRegisterWithDocker "fs1" "etc var/lib var/log supervisord shares"
-
+if [ "X$1" == "--help" ]; then
+  usage
+elif [ "X$1" == "Xaddc" ]; then
+  # dc1 directories
+  BASE="${2:-dc1}"
+  echo hostname: $BASE 
+  createDirsAndRegisterWithDocker "$2" "etc var/lib var/log"
+elif [ "X$1" == "Xmember" ]; then
+  # fileserver directories
+  createDirsAndRegisterWithDocker "$2" "etc var/lib var/log supervisord shares"
+else
+  echo "Unknown Option: >$1<"
+  echo "Use --help to see options"
+  exit 1
+fi
 exit 0
 
