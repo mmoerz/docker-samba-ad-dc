ac17fa29283ae42da77956bdaa9593159003ed20
Michael Moerz <michael@moerz.co.at>
2024-01-03 13:39:28 +0100

    * fixed root access script for dc1

--- root_samba.sh
+++ root_samba.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 BASENAME=$(basename $(pwd))
+
+if [ $(grep "samba:" docker-compose.yml) != "" ]; then
+  echo "dc1 found"
+  BASENAME=${BASENAME}-samba
+fi
+
 echo $BASENAME
 DOCKER_ID=$(docker container list | grep "$BASENAME" | grep -v "member" | cut -d' ' -f1)
 
-docker exec -it $DOCKER_ID /bin/bash
+echo ">$DOCKER_ID<"
+docker exec -it "$DOCKER_ID" /bin/bash
