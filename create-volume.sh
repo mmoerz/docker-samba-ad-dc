90dac9a8202341d5a4b59a97baaaa8b73e917736
Michael Moerz <michae@moerz.co.at>
2024-10-02 15:31:25 +0200

    fixing volume creation for cups

--- create-volume.sh
+++ create-volume.sh
@@ -43,7 +43,7 @@ elif [ "X$1" == "Xmember" ]; then
   createDirsAndRegisterWithDocker "$2" "etc var/lib var/log supervisord shares"
 elif [ "X$1" == "Xcups" ]; then
   # fileserver directories
-  createDirsAndRegisterWithDocker "$2" "etc var/lib var/log supervisord shares"
+  createDirsAndRegisterWithDocker "$2" "etc/samba etc/cups var/lib var/log supervisord shares"
 else
   echo "Unknown Option: >$1<"
   echo "Use --help to see options"
