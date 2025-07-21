f3a2c577e0caf977d427f173f9fe291ba51942dd
Michael Moerz <michael@moerz.co.at>
2025-07-21 20:34:55 +0200

    fixed build bug, improved docker vol and net creation

--- utils/create-network.sh
+++ utils/create-network.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 # This is a pre-flight scritp to setup the network 
 #
-source ./samba.env
+source functions
 
+INTERFACE=$(ip link list | sed -ne 's/[0-9]\+:\s\(br[0-9]\+\)/\1/p')
+
+if [ "X$INTERFACE" = "X" ]; then
+  INTERFACE=$(ip link list | sed -ne 's/[0-9]\+:\s\(enp[0-9]\+s[0-9]\+\).*/\1/p')
+fi
+
+if [ "X$INTERFACE" = "X" ]; then
+  echo "missing interface for network"
+cat <<EOF
+USAGE: $0 [interface]
+
+       interface  
+EOF
+fi
+
+echo "Network inferace: >$INTERFACE<"
 # echo 
 docker network create -d macvlan \
     --subnet=$SUBNET \
     --gateway=$GATEWAY  \
     --ip-range=$SUBNET \
-    -o parent=br0 samba-network
+    -o parent=$INTERFACE samba-network
 
+docker network inspect samba-network
