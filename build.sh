fac941eacc11d9dbc1b36a0cf33e347d870c457b
Michael Moerz <michael@moerz.co.at>
2025-07-21 20:33:42 +0200

    fixed build, improved docker vol and net creation

--- build.sh
+++ build.sh
@@ -10,8 +10,14 @@ build_date=$(date -Iseconds)
 tagdate=$(date +%Y_%m_%d__%H%M)
 gituser=$(git config --global user.name)
 
+echo "version: $version"
+echo "build_date: $build_date"
+echo "tagdate: $tagdate"
+echo "gituser: $gituser"
+echo "user: $USER"
+
 docker build \
         --build-arg "version=$version" \
         --build-arg "build_date=$build_date" \
-        -t $gituser/docker-samba-ad-dc:${samba_version} smb-ad-dc
-docker tag $gituser/docker-samba-ad-dc:${samba_version} $gituser/docker-samba-ad-ac:latest
+        -t $USER/docker-samba-ad-dc:${samba_version} smb-ad-dc
+docker tag $USER/docker-samba-ad-dc:${samba_version} $USER/docker-samba-ad-ac:latest
